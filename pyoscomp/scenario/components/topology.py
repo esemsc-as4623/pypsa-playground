2126ea577cfd7c631f1f8137c6243c421ebf8bb3
Arushi Sinha <115908316+arushisinha98@users.noreply.github.com>
2026-01-06 14:57:36 +0000

    clean topology class structure with same function definitions as time

--- pyoscomp/scenario/components/topology.py
+++ pyoscomp/scenario/components/topology.py
@@ -6,10 +6,49 @@ Note: This component handles the creation of nodes for the scenario.
 Nodes are referred to as REGION in OSeMOSYS.
 Nodes are referered to as Buses in PyPSA.
 """
+import pandas as pd
+
 from .base import ScenarioComponent
 
 class TopologyComponent(ScenarioComponent):
-    def process(self, nodes_input):
+    """
+    Topology component for node definitions.
+    Handels OSeMOSYS REGION creation.
+
+    Prerequisites:
+        - None
+
+    Example usage::
+        topology = TopologyComponent(scenario_dir)
+        nodes = topology.add_nodes(5)  # Creates 5 generic nodes
+        # or
+        nodes = topology.add_nodes(['NodeA', 'NodeB', 'NodeC'])  # Uses specified names
+    """
+    def __init__(self, scenario_dir):
+        super().__init__(scenario_dir)
+
+        # Node parameters
+        self.regions_df = pd.DataFrame(columns=["REGION"])
+
+    # === Load and Save Methods ===
+    def load(self):
+        """
+        Load topology parameter CSV files into DataFrames.
+        Uses read_csv from base class.
+        :raises FileNotFoundError if any required file is missing.
+        :raises ValueError if any file has missing or incorrect columns.
+        """
+        self.regions_df = self.read_csv("REGION.csv")
+
+    def save(self):
+        """
+        Save topology parameter DataFrames to CSV files in the scenario directory.
+        Uses write_csv and write_dataframe from base class.
+        """
+        self.write_dataframe("REGION.csv", self.regions_df)
+
+    # === User Input Methods ===
+    def add_nodes(self, nodes_input):
         """
         Generates the list of nodes and writes to REGION.csv.
 
@@ -26,8 +65,5 @@ class TopologyComponent(ScenarioComponent):
             node_list = [str(n) for n in nodes_input]
         else:
             raise ValueError("nodes_input must be an integer or a list of strings.")
-
-        # Save to REGION.csv in the correct directory
-        self.write_csv("REGION.csv", node_list)
         
-        return node_list
+        self.regions_df = pd.DataFrame({"REGION": node_list})
