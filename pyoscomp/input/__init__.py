290f204f39b882162b7dceee0199e9e5a84ae938
Arushi Sinha <115908316+arushisinha98@users.noreply.github.com>
2026-03-24 13:01:31 +0800

    remove data discretization and factorization modules

--- pyoscomp/input/__init__.py
+++ pyoscomp/input/__init__.py
@@ -1,7 +1,1 @@
 # pyoscomp/input/__init__.py
-
-"""
-Input data handling submodule for PyPSA-OSeMOSYS Comparison Framework.
-"""
-from .reader import ScenarioInputReader
-from .structures import TimesliceStructure
--- pyoscomp/input/factorize/__init__.py
+++
@@ -1,7 +0,0 @@
-# pyoscomp/input/factorize/__init__.py
-
-"""
-Input factorization submodule for PyPSA-OSeMOSYS Comparison Framework.
-"""
-
-from .base import TimesliceGenerator
